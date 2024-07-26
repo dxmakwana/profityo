101812bfb0de415324739021fc1bfe66f38d2589
dxmakwana <155942696+dxmakwana@users.noreply.github.com>
2024-07-26 14:43:43 +0530

    salestax crud is done

---
+++ app/Models/SalesTax.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+// use App\Models\AdminMenu;
+
+class SalesTax extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id','tax_name', 'tax_abbreviation', 'tax_number', 'tax_desc', 'tax_number_invoices', 'tax_recoverable', 'tax_compound', 'tax_rate'];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        // Dynamically set the table name
+        $user = Auth::guard('masteradmins')->user();
+        $uniq_id = $user->buss_unique_id;
+        $this->setTable($uniq_id . '_py_sales_tax');
+    }
+}
